0c65b8164918d4eb58705a0b1b6470c26ada337f
PC-LAB-AUL-07 AULARIO-LAB-COMPUTACION <PC-LAB-AUL-07@pucesm.edu.ec>
2024-05-15 09:30:20 -0500

    Correccion de comentarios

--- 05_manipulacion_string.js
+++ 05_manipulacion_string.js
@@ -19,16 +19,16 @@ const text = 'Hola mundo de Software'
 console.log(text[2]) // El [2] indico el indice que deseo mostrar, en este caso seria la letra l.
 console.log(text.charAt(2)) // El metodo charAT(), haria lo mismo que el anterior.
 console.log(text.indexOf('a')) // El metodo indexOf() te muestra donde esta ubicado el index, en este caso es en el 3.
-console.log(text.indexOf('Hola'))// En este ejemplo daria desde donde comienza.
+console.log(text.indexOf('Hola'))// En este ejemplo daria desde donde comienza. El resultado daria 0 poruqe desde ahi comienza.
 console.log(text.lastIndexOf('o'))// El metodo lastIndexIf(), indica donde esta ubicado la ultima letra que uno escriba. En este caso mostraria 15.
 console.log(text.slice(0, 4))// El metodo slice(), recibe dos parametros para ver desde y hasta donde quieres mostrar. En este caso mostraria Hola.
-console.log(text.split(' '))// El metodo split(), separa las palabras de acuerdo a lo que le indiques, en este a partir de los espacios que existen.
-console.log(text.toLocaleUpperCase())// El metodo toLocaleUpperCase(), hace mayuscula todo el string.
-console.log(text.toLocaleLowerCase())// El metodo toLocaleLoweCase(), hace minuscula todo el string.
+console.log(text.split(' '))// El metodo split(), separa las palabras de acuerdo a lo que le indiques, en este a partir de los espacios que existen. El resultado seria [ 'Hola', 'mundo', 'de', 'Software' ]
+console.log(text.toLocaleUpperCase())// El metodo toLocaleUpperCase(), hace mayuscula todo el string. El resultado seria 'HOLA MUNDO DE SOFTWARE'
+console.log(text.toLocaleLowerCase())// El metodo toLocaleLoweCase(), hace minuscula todo el string. El resultado serua 'hola mundo de software' 
 
 // Otro ejemplo de manipulacion
 const text2 =  ' Hola Mundo de Software '
-console.log(text2.trim())// El metodo trim(), elimina esos espacios que llegan a existir en el string.
-console.log(text2.replace('Software', '💻'))// El metodo replace(), indicas lo que deseas reemplazar si llegase a existir, y indicas con que lo reemplazaria.
-
+console.log(text2.trim())// El metodo trim(), elimina esos espacios que llegan a existir en el string. El resultado es el siguiente 'Hola Mundo de Software'
+console.log(text2.replace('Software', '💻'))// El metodo replace(), indicas lo que deseas reemplazar si llegase a existir, 
+                                            // y indicas con que lo reemplazaria. El resultado seria el siguiente ' Hola Mundo de 💻 '
 
