f4d86cf2203471bf96a6b3499b2acc3f2976871a
Miguelzea12 <aroryt02@gmail.com>
2024-05-22 14:33:54 -0500

    30 complete

--- Unidad_4/30_slice_reverse.js
+++ Unidad_4/30_slice_reverse.js
@@ -5,3 +5,30 @@ const sliceFruit = fruit.splice(2, 3, 'Uva', 'Uva verde');
 
 console.log(sliceFruit);
 console.log(fruit);
+
+// Metodo reverse().
+
+const num = [1, 5, 16, 89, 22, 44];
+const numreverse = num.reverse();
+
+console.log(numreverse);
+
+
+// Metodo sort();
+
+const num1 = [1, 5, 16, 89, 22, 44];
+const numSort = num.sort((A, B) => A - B );
+console.log(numSort);
+
+// Metodo sort() white string
+
+const name = ['Miguel', 'Alejandro', 'Xavier', 'Aldair', 'Daza'];
+const nameSort = name.sort();
+console.log(nameSort);
+
+// Metodo fill().
+
+const num2 = [1, 5, 16, 89, 22, 44];
+const num2Fill = num2.fill(0, 2, 4);
+
+console.log(num2Fill);
