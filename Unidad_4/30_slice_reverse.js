26f6e0bcf7766f88907c5049d89df620f5e5d8e6
Miguelzea12 <aroryt02@gmail.com>
2024-05-22 14:38:40 -0500

    correcciones

--- Unidad_4/30_slice_reverse.js
+++ Unidad_4/30_slice_reverse.js
@@ -3,32 +3,32 @@
 const fruit = ['Manzana', 'Pera', 'Piña', 'Sandia'];
 const sliceFruit = fruit.splice(2, 3, 'Uva', 'Uva verde');
 
-console.log(sliceFruit);
-console.log(fruit);
+console.log(sliceFruit); // [ 'Piña', 'Sandia' ]
+console.log(fruit); // [ 'Manzana', 'Pera', 'Uva', 'Uva verde' ]
 
 // Metodo reverse().
 
 const num = [1, 5, 16, 89, 22, 44];
 const numreverse = num.reverse();
 
-console.log(numreverse);
+console.log(numreverse);// [ 44, 22, 89, 16, 5, 1 ]
 
 
 // Metodo sort();
 
 const num1 = [1, 5, 16, 89, 22, 44];
 const numSort = num.sort((A, B) => A - B );
-console.log(numSort);
+console.log(numSort); // [ 1, 5, 16, 22, 44, 89 ]
 
 // Metodo sort() white string
 
 const name = ['Miguel', 'Alejandro', 'Xavier', 'Aldair', 'Daza'];
-const nameSort = name.sort();
-console.log(nameSort);
+const nameSort = name.sort(); 
+console.log(nameSort); // [ 'Aldair', 'Alejandro', 'Daza', 'Miguel', 'Xavier' ]
 
 // Metodo fill().
 
 const num2 = [1, 5, 16, 89, 22, 44];
 const num2Fill = num2.fill(0, 2, 4);
 
-console.log(num2Fill);
+console.log(num2Fill); // [ 1, 5, 0, 0, 22, 44 ]
