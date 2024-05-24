4d84c8cadfe01e83df0c523359e5ca048a87a458
MiguelZea12 <aroryt02@gmail.com>
2024-05-23 21:48:05 -0500

    Complete

--- Unidad_4/Project_4/ejercicio.js
+++ Unidad_4/Project_4/ejercicio.js
@@ -18,36 +18,48 @@ function action() {
 
 function app(action) {
     switch(action) {
+
+        // El primer caso por si el usuario llegase a querer añadir un libro.
         case 'add':
-            const newBook = prompt('Ingresa el nombre del libro que deseas añadir: ').toLocaleLowerCase(); // Ingresa el nombre del libro que quiera guardad.
+            const newBook = prompt('Ingresa el nombre del libro que deseas añadir: ').toLocaleLowerCase(); 
             if (books.includes(newBook)) {
-                alert(`El libro ${newBook} ya está en el stack`); // Si el libro se repite le da una alerta de que ya existe.
+                alert(`El libro ${newBook} ya está en el stack`); 
             } else {
                 books.push(newBook);
-                alert(`Ya se ha incluido el libro ${newBook} al stack`); // Le da una alerta de que ya se almaceno el libro.
+                alert(`Ya se ha incluido el libro ${newBook} al stack`); 
             }
-            break; // Se termina el primer caso,        
+            break;        
+
+        // El segundo caso por  si el usuario llegase a querer eliminar un libro.
         case 'remove':
             if (books.length > 0) {
-                books.pop(); // Si la cantidad de libros supera la de 0, entonces elimina el ultimo elemento,
+                books.pop(); 
             } else {
-                alert('No hay libros para eliminar'); // si llega a ser 0, le da una alerta de que no hay libro para eliminar.
+                alert('No hay libros para eliminar'); 
             }
-            break;// Se corta el segundo caso.
+            break;
+
+        // El tercer caso por si el usuario llegase a querer ver todos los libros de su Stack.
         case 'all':
-            console.log(books); // Muestra todos los libros.
-            break; // Se corta el tercer caso.
+            console.log(books); 
+            break; 
+        
+        // El cuarto caso por si el usuario quiere salir de la app.
         case 'exit':
-            console.log('Adios'); // Se despide cuando el usuario quiere teminar.
-            return false;  // Indicamos que queremos salir del bucle
+            console.log('Adios'); 
+            return false;  
+        
+        // Caso default por si llega a escribir una opcion que no este determinada.
         default:
-            alert('No escogiste una opción válida'); // Si no escogio una de las opciones, le muestra que no escogio una opcion valida.
+            alert('No escogiste una opción válida');
     }
-    return true;  // Indicamos que queremos seguir en el bucle
+    return true;  
 }
-let books = []; // Array vacio para que se almacenen.
-let running = true; // Variable para que siga el bucle.
+
+// Loop para correr la app.
+let books = []; 
+let running = true; 
 while (running) {
-    const userAction = action(); // Asignamos la funcion action() a una variable.
-    running = app(userAction); // Con running le asignamos la funcion app con la variable userAction para que reciba los datos.
+    const userAction = action(); 
+    running = app(userAction); 
 }
